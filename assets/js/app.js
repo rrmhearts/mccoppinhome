0669871a3d37ddfa482adfec852c5395cad50a59
Ryan <rrmhearts@gmail.com>
2020-04-09 17:52:26 -0400

    highlight sidenav links.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,36 @@
-function openNav() {
+document.body.addEventListener('click', e => {
+     
+    const href = e.target.href;
+    if (!href) return;
+   
+    const id = href.split('#').pop();
+
+    const navlink = id + '_nav';
+    const target = document.getElementById(navlink);
+    const links = document.querySelectorAll('.nav-link');
+    console.log('links: ', links);
+    links.forEach(link => {
+        console.log(link)
+        link.classList.remove('active');
+    });
+
+    if (!target) return;
+
+    target.classList.add('active');
+
+    /*
+        I'm using css instead.
+    */
+    //e.preventDefault();
+    // history.pushState({}, document.title, href);
+    // target.scrollIntoView({
+    //   behavior: 'smooth',
+    //   block: 'start'
+    // });
+
+  });
+  
+  function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("topbar").style.marginLeft = "250px";
