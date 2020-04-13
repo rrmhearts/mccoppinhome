1ffab613a2b36d3b6dca08f9ca9028a8c5e48b9b
Ryan <rrmhearts@gmail.com>
2020-04-13 09:20:50 -0400

    scroll finesse. Full screen, open sidebar.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,21 @@
+
+const sections = document.querySelectorAll('section');
+const links = document.querySelectorAll('.nav-link');
+
+window.onload = (event) => {
+   // window.scrollTo(0,0)
+}
+
 document.body.addEventListener('click', e => {
-     
+    window.removeEventListener('scroll', changeLinkState);
+
     const href = e.target.href;
     if (!href) return;
-   
+
     const id = href.split('#').pop();
 
     const navlink = id + '_nav';
     const target = document.getElementById(navlink);
-    const links = document.querySelectorAll('.nav-link');
     console.log('links: ', links);
     links.forEach(link => {
         console.log(link)
@@ -27,30 +35,52 @@ document.body.addEventListener('click', e => {
     //   behavior: 'smooth',
     //   block: 'start'
     // });
+    setTimeout(() => {
+        window.addEventListener('scroll', changeLinkState);
 
-  });
-  
-  function openNav() {
+    }, 1000)
+});
+
+function changeLinkState() {
+    let index = sections.length;
+
+    while (--index && window.scrollY + (index > 0 ? -800 : -500) < sections[index].offsetTop) { }
+    console.log(index, " ", window.scrollY, " ", sections[index].offsetTop);
+//560    446  d 114
+//1500   602  d 898
+//2410  1658  d 752
+//3310  2714  d 596
+console.log(links)
+    links.forEach((link) => link.classList.remove('active'));
+    if (window.scrollY > 460)
+        links[index].classList.add('active');
+}
+
+window.addEventListener('scroll', changeLinkState);
+
+function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("topbar").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("topbar").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-  }
-  
-  function toggle() {
+}
+
+function closeNav() {
+    if (screen.width < 1024) {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        document.getElementById("topbar").style.marginLeft = "0";
+        document.body.style.backgroundColor = "white";
+    }
+}
+
+function toggle() {
     let side = document.getElementById("mySidenav");
     let style = window.getComputedStyle(side);
     let width = style.getPropertyValue('width');
     console.log("toggle: ", width);
-    if (width === "0px") 
+    if (width === "0px")
         openNav();
     else
         closeNav();
-  }
+}
